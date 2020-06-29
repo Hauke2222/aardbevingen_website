2c43d06234606985603e2b8eab4c47f5e57a2df3
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2020-06-29 19:07:33 +0200

    addes display function

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 const urlInduced = 'http://cdn.knmi.nl/knmi/map/page/seismologie/all_induced.json';
 const urlTectonic = 'http://cdn.knmi.nl/knmi/map/page/seismologie/all_tectonic.json';
+parsedInduced = [];
 
 async function getInduced() {
     const response = await fetch(urlInduced);
-    const parsedInduced = await response.json();
+    parsedInduced = await response.json();
     console.log(parsedInduced);
+    return parsedInduced;
 
 }
 
-
-getInduced();
-  
 function displayData() {
-
+    for (let i = 0; i < parsedInduced.length; i++) {
+        console.log(test);
+        
+        let inducedEvent = parsedInduced[i];
+        let div = document.createElement('div');
+        div.innerHTML = inducedEvent.date;
+        document.body.appendChild(div);
+    }
 }
+getInduced();
+displayData();
 
-obj.events[0].date + " " + obj.events[0].place;
+
+//obj.events[0].date + " " + obj.events[0].place;
