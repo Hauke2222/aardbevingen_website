bb4c4f40a456c2ae394bdd45e0e76757bf3c90c1
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2020-06-30 22:39:26 +0200

    "json in bootstrap table"

--- main.js
+++ main.js
@@ -10,10 +10,29 @@ async function getInduced() {
 }
 
 function displayData() {
-    for (let i = 0; i < parsedInduced["events"].length; i++) {
-        let div = document.createElement('div');
-        div.innerHTML = parsedInduced["events"][i].date +  parsedInduced["events"][i].place +  parsedInduced["events"][i].mag;
-        document.body.appendChild(div);
-    }
+
+    var events = parsedInduced.events;
+    events.map((value, index) => {
+
+        console.log(index, value);
+
+        var table = document.getElementById("mytable");
+        var row = table.insertRow(index + 1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = value.date;
+        cell2.innerHTML = value.place;
+        cell3.innerHTML = value.mag;
+
+
+    });
+
+
+    // for (let i = 0; i < parsedInduced["events"].length; i++) {
+    //    let div = document.createElement('div');
+    //   div.innerHTML = parsedInduced["events"][i].date +  parsedInduced["events"][i].place +  parsedInduced["events"][i].mag;
+    //   document.body.appendChild(div);
+    //}
 }
-getInduced();
+getInduced(); 
